c90537ecd3e20867fe52ad1ad05eee1f5f87a2e9
Aze-M <archmageazekil@gmail.com>
2024-09-26 13:41:27 +0200

    update init of items

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from src.objs import item                       # This imports the player and it
 from src.utils import damage_calc               # This imports the damage_calc module from the src.utils package
 
 if __name__ == "__main__":                      # This line ensures that the code block is only executed if the script is run directly, not if it is imported as a module in another script.
-    basE_item_test = item.ItemBase(1,1,"Test")  # This creates an instance of the ItemBase class from the item module with the parameters 1, 1, "Test".
+    basE_item_test = item.Weapon(1,1,"Test",[0])  # This creates an instance of the ItemBase class from the item module with the parameters 1, 1, "Test".
     print (basE_item_test)                      # This prints the string representation of the basE_item_test object.
--- src/objs/item.py
+++ src/objs/item.py
@@ -2,12 +2,13 @@
 class ItemBase:
     _id: int #Item reference id for items.json
     _rarity: int #raritys tbd
+    _item_type: int # item type (0 armor, 1 mainhand, 2 offhand)
     _name: str #important
     _weight: float #might go unused will see
     _upgrade_level: int #for upgrade system, might change
     _stat_bonus: list #should be fed as (stat, bonus) lists in list , as in (strength, 2)
 
-    def __init__(self, _new_id, _rarity, _name, _load_data=None):
+    def __init__(self, _new_id, _rarity_init, _name_init, _load_data=None):
         #ID is absolutely required for saving the item, mirror in item.JSON, init inherits down
         if not _new_id:
             raise ValueError("Needs ID or can not initialize item!")
@@ -15,20 +16,25 @@ class ItemBase:
         self._id = _new_id
 
         #default rest if not available - Replace with loading code later
-        if _rarity:
-            self._rarity = _rarity
+        if _rarity_init:
+            self._rarity = _rarity_init
         else:
-            _rarity = 0
+            _rarity_init = 0
 
-        if _name:
-            self._name = _name
+        if _name_init:
+            self._name = _name_init
         else:
             self._name = "Item"
 
         #If we are loading a serialized item pass in the load data (Serialized)
         #Add deserialization when we have serialization
-        if _load_data:
-            pass
+
+        self._item_type = 0
+
+        if self._item_type == 0:
+            self.load_data()
+
+
 
 class Weapon(ItemBase):
     _damage: float
@@ -36,9 +42,15 @@ class Weapon(ItemBase):
     _crit_chance: float #in %
     _crit_multi: float #as multiplier
 
+    def load_data(self):
+        pass
+
 class Armor(ItemBase):
     _armour: float #as damage reduction % e.g. 0.25 = 25%
     _armour_type: int #0 for heavy, 1 for medium , 2 for light
+
+    def load_data():
+        pass
 
 class OffHand(ItemBase):
     _offhand_armour: float #additional damage reistance as percent float, eg 0.25 = 25%
