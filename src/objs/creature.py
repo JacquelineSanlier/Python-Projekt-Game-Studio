bfdbcbd331801939d74291a3e4cc8df3ca895d44
FlashBier <jeff.helm1991@yahoo.de>
2024-09-26 14:45:17 +0200

    player settings for day 1

--- src/objs/creature.py
+++ src/objs/creature.py
@@ -1,13 +1,16 @@
 class CreatureBase:
     _id: int
-    _health
+    _health: int
+    _name: str
+    _attack_power: int
+    _max_HP: int
 
     def __init__(self, _id: int, _name: str, _health: int, _attack_power: int, _max_HP: int):
         self._id = _id
-        self.name = _name
-        self.health = _health
-        self.attack_power = _attack_power
-        self.max_HP = _max_HP
+        self._name = _name
+        self._health = _health
+        self._attack_power = _attack_power
+        self._max_HP = _max_HP
 
     def get_id(self):
         return self._id
@@ -17,12 +20,11 @@ class Player(CreatureBase):
     def __init__(self, _name: str, _health: int, _attack_power: int, _max_HP: int):
         super().__init__(0, _name, _health, _attack_power, _max_HP)
 
-    def take_damage(self, _damage: int):
-        self.health -= _damage
+    def check_health(self):
         if self.health > self.max_HP:
             self.health = self.max_HP
         elif self.health <= 0:
             self.die()
 
 
-# inventar, offhand, armoury
+# add space for inventary, offhand, armoury, main_hand
