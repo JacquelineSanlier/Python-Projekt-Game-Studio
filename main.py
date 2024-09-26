ff291c85b85ebfc4e9f20ecd7ddf48a2f741ba85
Aze-M <archmageazekil@gmail.com>
2024-09-26 10:38:08 +0200

    move and adjust import

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
-from src import player
-from src.utils import damage_calc
+from src.objs import player
 from src.objs import item
+from src.utils import damage_calc
 
 if __name__ == "__main__":
     basE_item_test = item.ItemBase(1,1,"Test")
