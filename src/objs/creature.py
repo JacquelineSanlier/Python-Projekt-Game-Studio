553eeb9a919c6e61a17e77e363d66a008c0e5596
FlashBier <jeff.helm1991@yahoo.de>
2024-09-26 14:41:59 +0200

    add

--- src/objs/creature.py
+++ src/objs/creature.py
@@ -1,27 +1,28 @@
 class CreatureBase:
     _id: int
+    _health
 
-    def __init__(self, _id: int):
-        self._id = id
+    def __init__(self, _id: int, _name: str, _health: int, _attack_power: int, _max_HP: int):
+        self._id = _id
+        self.name = _name
+        self.health = _health
+        self.attack_power = _attack_power
+        self.max_HP = _max_HP
 
     def get_id(self):
         return self._id
 
-        class Player(CreatureBase):
-    def __init__(self, _name: str, _health: int, _attack_power: int, _class: int):
-        super().__init__(0)
-        self.name = name
-        self.health = health
-        self.attack_power = attack_power
+class Player(CreatureBase):
+    
+    def __init__(self, _name: str, _health: int, _attack_power: int, _max_HP: int):
+        super().__init__(0, _name, _health, _attack_power, _max_HP)
 
     def take_damage(self, _damage: int):
-        self.health -= damage
-        if self.health <= 0:
+        self.health -= _damage
+        if self.health > self.max_HP:
+            self.health = self.max_HP
+        elif self.health <= 0:
             self.die()
 
-    def die(self):
-        print(f"{self.name} has been defeated.")
 
-    def heal(self, _amount: int):
-        self.health += amount
-        print(f"{self.name} has been healed by {amount} points.")    
+# inventar, offhand, armoury
