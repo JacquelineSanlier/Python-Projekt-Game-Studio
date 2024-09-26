1b390e5ef87c2e98b39ad5698bd6db3b416b8cff
Aze-M <archmageazekil@gmail.com>
2024-09-26 10:36:56 +0200

    commenting item type

--- src/objs/item.py
+++ src/objs/item.py
@@ -1,11 +1,11 @@
 #Inherits down to other types
 class ItemBase:
-    _id: int
-    _rarity: int
-    _name: str
-    _weight: float
-    _upgrade_level: int
-    _stat_bonus: list
+    _id: int #Item reference id for items.json
+    _rarity: int #raritys tbd
+    _name: str #important
+    _weight: float #might go unused will see
+    _upgrade_level: int #for upgrade system, might change
+    _stat_bonus: list #should be fed as (stat, bonus) lists in list , as in (strength, 2)
 
     def __init__(self, _new_id, _rarity, _name, _load_data):
         #ID is absolutely required for saving the item, mirror in item.JSON, init inherits down
@@ -30,14 +30,14 @@ class ItemBase:
 
 class Weapon(ItemBase):
     _damage: float
-    _damage_type: int
-    _crit_chance: float
-    _crit_multi: float
+    _damage_type: int #0 for physical, 1 for magical
+    _crit_chance: float #in %
+    _crit_multi: float #as multiplier
 
 class Armor(ItemBase):
-    _armour: float
-    _armour_type: int
+    _armour: float #as damage reduction % e.g. 0.25 = 25%
+    _armour_type: int #0 for heavy, 1 for medium , 2 for light
 
 class OffHand(ItemBase):
-    _offhand_armour: float
-    _offhand_damage_mult: float
+    _offhand_armour: float #additional damage reistance as percent float, eg 0.25 = 25%
+    _offhand_damage_mult: float #damage multiplier as multiplier e.g. 1.10 = 10% more
