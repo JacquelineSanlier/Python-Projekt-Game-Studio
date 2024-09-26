adeb333227567cd04b0a14d848c6587ff0b0b794
Aze-M <archmageazekil@gmail.com>
2024-09-26 14:30:35 +0200

    prep for later loading

--- src/objs/item.py
+++ src/objs/item.py
@@ -6,6 +6,7 @@ class ItemBase:
     _weight: float #might go unused will see
     _upgrade_level: int #for upgrade system, might change
     _stat_bonus: list #should be fed as (stat, bonus) lists in list , as in (strength, 2)
+    _equpped: bool #whether or not the item is currently equipped
 
     def __init__(self, _new_id, _rarity_init = 0, _name_init = "Item", _load_data=None):
         #ID is absolutely required for saving the item, mirror in item.JSON, init inherits down
@@ -20,19 +21,29 @@ class Weapon(ItemBase):
     _crit_chance: float #in %
     _crit_multi: float #as multiplier
 
-    def load_data_weapon(self):
-        pass
+    def __init__(self, _new_id, _rarity_init=0, _name_init="Item", _load_data=None):
+        super().__init__(_new_id, _rarity_init, _name_init, _load_data)
+
+        #deserialize load data here
+        
+
 
 class Armor(ItemBase):
     _armour: float #as damage reduction % e.g. 0.25 = 25%
     _armour_type: int #0 for heavy, 1 for medium , 2 for light
 
-    def load_data_armor(self):
-        pass
+    
+    def __init__(self, _new_id, _rarity_init=0, _name_init="Item", _load_data=None):
+        super().__init__(_new_id, _rarity_init, _name_init, _load_data)
+
+        #deserialize load data here
 
 class OffHand(ItemBase):
     _offhand_armour: float #additional damage reistance as percent float, eg 0.25 = 25%
     _offhand_damage_mult: float #damage multiplier as multiplier e.g. 1.10 = 10% more
 
-    def load_data_offfhand(self):
-        pass
+    
+    def __init__(self, _new_id, _rarity_init=0, _name_init="Item", _load_data=None):
+        super().__init__(_new_id, _rarity_init, _name_init, _load_data)
+
+        #deserialize load data here
