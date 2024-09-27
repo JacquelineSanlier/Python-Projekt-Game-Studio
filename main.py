c43bdf0954123bf52330e1931cf0b6747d825ce4
Aze-M <archmageazekil@gmail.com>
2024-09-27 13:17:16 +0200

    remove unneccessary old test code

--- main.py
+++ main.py
@@ -51,7 +51,3 @@ while is_running:
     clock.tick(FPS)
     
 pygame.quit()
-
-if __name__ == "__main__":                      # This line ensures that the code block is only executed if the script is run directly, not if it is imported as a module in another script.
-    basE_item_test = item.Weapon(1,1,"Test",[0])  # This creates an instance of the ItemBase class from the item module with the parameters 1, 1, "Test".
-    print (basE_item_test)                      # This prints the string representation of the basE_item_test object.
