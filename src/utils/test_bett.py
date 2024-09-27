056f5a7c7b5c48d0953f04e2e4fd2f64f9ee43e8
joergdeymann <joergdeymann@web.de>
2024-09-27 16:10:48 +0200

    Tiles with Doc

--- src/utils/test_bett.py
+++ src/utils/test_bett.py
@@ -1,8 +1,8 @@
 import pygame
-import ui
+import tiles as tiles
 
 main_entity=[]
-
+unique_entities=[]
 
 # pygame setup
 pygame.init()
@@ -13,18 +13,48 @@ dclocktime = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-u = ui.screen_tile(pygame, screen, 1, None)
-u._getX()
-u.getY()
-exit()
+#t=tiles.tile_item(pygame,1,"Tree",'./assets/svg/tree.svg')
+#unique_entries=tile_list.getImage()
+t=tiles.tile_item(pygame,1,"Tree",'./assets/svg/tree.svg')
+unique_entities.append(t)
 
-u=ui.screen_tile(pygame,screen,1,None)
+i=unique_entities[0].getImage()
+
+tree=unique_entities[0]
+
+u=tiles.screen_tile(pygame,screen,1,unique_entities[0])
 u.setPosition(100,100)
 u.setSize(50,75)
-u.setHitbox(105,105,40,60)
+#u.setHitbox(105,105,40,60)
 main_entity.append(u)
-u.getX()
-print (main_entity[0].getX())
+
+
+
+u=tiles.screen_tile(pygame,screen,2,tree)
+u.setPosition(200,100)
+u.setSize(100,20)
+#u.setHitbox(105,105,40,60)
+main_entity.append(u)
+
+u=tiles.screen_tile(pygame,screen,3,tree)
+u.setPosition(300,150)
+u.setSize(20,20)
+#u.setHitbox(105,105,40,60)
+main_entity.append(u)
+
+u=tiles.screen_tile(pygame,screen,4,tree)
+u.setPosition(100,170)
+u.setSize(20,200)
+#u.setHitbox(105,105,40,60)
+main_entity.append(u)
+
+u=tiles.screen_tile(pygame,screen,5,tree)
+u.setPosition(300,200)
+u.setSize(200,200)
+u.setHitbox(20,34,160,156)
+main_entity.append(u)
+
+
 #main_entity = [{
 #    ui:ui.screen_tile(pygame,screen,1,None),    
 #}]
@@ -37,11 +67,17 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    #screen.blit(unique_entities[0].getImage(), (100, 150)) 
+    #u._draw_image() 
     
-    # main_entity[0].update()
+    #screen.blit(t.getImage(), (200, 150)) 
+
+    # pygame.draw.circle(screen, "red", player_pos, 40)
+
+    for entity in main_entity:
+        entity.update()
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
--- src/utils/tiles.py
+++ src/utils/tiles.py
@@ -7,49 +7,61 @@
 # we cann add movements,sequences  here or better in an other class where like:
 # dieing, jumping, falling, landing, walking 
 # 
+# import os
 
 class tile_item:
-	#_id:int=0        # id 
-	#_src:str =""     # path
-	#_name:str =""    # name caan be found by Name  
-	
+    _id:int=0        # id 
+    _src:str =""     # path
+    _name:str =""    # name caan be found by Name  
+    _image = None     # 
 
-	#
-	# Initialise and give 
-	#
-    def __init__(self,id,name,src):
+    #
+    # Initialise and give 
+    #
+    def __init__(self,pygame,id,name,src):
         self._id    = id
         self._name  = name
         self._src   = src
+        self._image = self.__load(pygame,src)
+
+    def __load(self,pygame,src):
+        #pygame.image.load(os.path.join('data', src))
+        return pygame.image.load(src)
+
+    def getImage(self):
+        return self._image 
 
 	
 # Tile = Item auf dem Bildschirm , Entity = Interagierendes Item
+# you can resiz
+# you can move
+
 class screen_tile:  	
-	_pygame = None            #Pygame
-	_screen = None            #Screen whehre it is displayed
-	_id:int = 0               #unique ID
-	_interactive:bool = False #0 Not inteeractive 1 with collision
-	_src: str = ""            #Image file
-	_name:str = ""            #ZB "Baum"
-	_x:int = 0                #Position X
-	_y:int = 0                #Position Y
-	_width:int = 0            #Width 
-	_height:int = 0           #Height
-	_scale:float = 1          #Scale Factor
-	_tile_item=None           #has the src of the Image
+    _pygame = None            #Pygame
+    _screen = None            #Screen whehre it is displayed
+    _id:int = 0               #unique ID
+    _interactive:bool = False #0 Not inteeractive 1 with collision
+    _src: str = ""            #Image file
+    _name:str = ""            #ZB "Baum"
+    _x:int = 0                #Position X
+    _y:int = 0                #Position Y
+    _width:int = 0            #Width 
+    _height:int = 0           #Height
+    _scale:float = 1          #Scale Factor
+    _tile=None                #has the src of the Image
 
 	
 	
-	_dx:int =0				  #movement per 1/60s  -1 left +1 right 0 no move
-	_dy:int =0                #movement per 1/60s  -1 up   +1 down  0 no move
-	
-	_hitbox_x:int = 0                #Collision Position X
-	_hitbox_y:int = 0                #Collision Position Y
-	_hitbox_width:int = 0            #Collision Width 
-	_hizbox_height:int = 0           #Collision Height
-	__TRANSPARENT = (0,0,0,0)        #Transparenter Hintergrund 0-255
-	__BLUE        = (0,0,255,255)    #Transparenter Hintergrund 0-255
-	__RED         = (255,0,0,255)    #Transparenter Hintergrund 0-255
+    _dx:int =0				  #movement per 1/60s  -1 left +1 right 0 no move
+    _dy:int =0                #movement per 1/60s  -1 up   +1 down  0 no move
+    _speed:float =1           #Speed for the Movement (Steps to move or times)
+    _hitbox_x:int = None                #Collision Position X
+    _hitbox_y:int = None                #Collision Position Y
+    _hitbox_width:int = None            #Collision Width 
+    _hitbox_height:int = None           #Collision Height
+    __TRANSPARENT = (0,0,0,0)        #Transparenter Hintergrund 0-255
+    __BLUE        = (255,255,0)    #Transparenter Hintergrund 0-255
+    __RED         = (255,0,0,128)    #Transparenter Hintergrund 0-255
 	
 	# Erstelle eine Surface mit Transparenz
 	#transparent_surface = pygame.Surface((rect_width, rect_height), pygame.SRCALPHA)
@@ -58,95 +70,111 @@ class screen_tile:
     #screen.blit(transparent_surface, (rect_x, rect_y))
     
 
-	def __init__(self,pygame,screen,id,tile_item=None):
-		self._id=id
-		self._tile_item=tile_item
-		self._pygame=pygame
-		self._screen=screen
+    def __init__(self,pygame,screen,id,tile=None):
+        self._id=id
+        self._tile=tile
+        self._pygame=pygame
+        self._screen=screen
 
-	def _getX(self):
-		print("Hallo")
-		print(self._x)
+    def setId(self,id):
+        self._id=id
 
-	def getY(self):
-		print("Hallo Y")
+    def setName(self,name):
+        self._name=name
 
-	def setId(self,id):
-		self._id=id
+    def setPosition(self,x,y):
+        self._x=x
+        self._y=y
+        if (self._hitbox_x == None):
+            self._hitbox_x=0
+        if (self._hitbox_y == None):
+            self._hitbox_y=0
 
-	def setName(self,name):
-		self._name=name
+    def setSize(self,width,height):
+        self._width=width
+        self._height=height
+        if (self._hitbox_width == None):
+            self._hitbox_width=width
+        if (self._hitbox_height == None):
+            self._hitbox_height=height
 
-	def setPosition(self,x,y):
-		self._x=x
-		self._y=y
-		self.hitbox_x=x
-		self.hitbox_y=y
+    def setHitbox(self,x,y,width,height):
+        self._hitbox_x=x
+        self._hitbox_y=y
+        self._hitbox_width=width
+        self._hitbox_height=height
 
-	def setSize(self,width,height):
-		self._width=width
-		self._height=height
-		self._hitbox_width=width
-		self._hitbox_height=height
+    def setScale(self,scale):
+        self._scale=scale
 
-	def setHitbox(self,x,y,width,height):
-		self.hitbox_x=x
-		self.hitbox_y=y
-		self._hitbox_width=width
-		self._hitbox_height=height
+    def setMove(self,dx,dy):
+        self._dx=dx*self._speed
+        self._dy=dy*self._speed
 
-	def setScale(self,scale):
-		self._scale=scale
-
-	def setMove(self,dx,dy):
-		self._dx=dx
-		self._dy=dy
-		
-	def move(x,y):
-
-		#remove old from screen
-		pass
-
-	def remove():
-		pass
-
-	def hide():
-		pass
-
-	def collides():
-		pass
+    def setSpeed(self,speed=1):
+        self._speed=speed
     
-	def _draw_rect(self):
-		x=self._x * self._scale
-		y=self._y * self._scale
-		w=self._width
-		h=self._height
-		screen=self._screen
-		# self._pygame.draw.rect(screen,self.__BLUE,(x,y,w,h),2)
-		self._pygame.draw.rect(screen,(0,0,255),(x,y,w,h))
+    def setTitle(self,tile):
+        self._tile=tile
 
-	def _draw_hitbox(self):
-		x=self._hitbox_x * self._scale
-		y=self._hitbox_y * self._scale
-		w=self._hitbox_width
-		h=self._hitbox_height
-		screen=self._screen
-		return
-		self._pygame.draw.rect(screen,self.__RED,(x,y,w,h),1)
+    def move(self,x,y):
+	    #remove old from screen or show animation
+        #move the tile
+        self._x+=(self._dx * self._speed)
+        self._y+=(self._dx * self._speed)        
 
-	def _draw_image(self):
-		return
-		x=self._x * self._scale
-		y=self._y * self._scale
-		w=self._width
-		h=self._height
-		screen=self._screen
+    def remove():
+        pass
 
-	def update(self):
-		return
-		self._draw_rect()
-		#self._draw_hitbox()
-		#self._draw_image()
+    def hide():
+        pass
+
+    def collides():
+        pass
+    
+    def _draw_rect(self):
+        x=self._x * self._scale
+        y=self._y * self._scale
+        w=self._width * self._scale
+        h=self._height * self._scale
+        screen=self._screen
+        # self._pygame.draw.rect(screen,self.__BLUE,(x,y,w,h),2)    
+        self._pygame.draw.rect(screen,self.__BLUE,(x,y,w,h),2)
+
+    def _draw_hitbox(self):
+        x=(self._x+self._hitbox_x) * self._scale
+        y=(self._y+self._hitbox_y) * self._scale
+        w=self._hitbox_width * self._scale
+        h=self._hitbox_height * self._scale
+        screen=self._screen
+        self._pygame.draw.rect(screen,self.__RED,(x,y,w,h),2)
+
+    def _draw_image(self):
+        # print(self._tile._name)
+        # screen=self._screen
+        #image=self._pygame.image.load('./assets/svg/tree.svg'); 
+        #image=self._pygame.image.load('./assets/svg/tree.svg'); 
+
+
+        # image=self._tile.getImage()
+        x=self._x * self._scale
+        y=self._y * self._scale
+        w=self._width
+        h=self._height
+        image=self._tile._image
+        image=self._pygame.transform.scale(image, (w, h))
+        #pygame.transform.rotate()
+        #rotated_image = pygame.transform.rotate(image, 45)
+        screen=self._screen
+        screen.blit(image, (x, y)) 
+
+
+
+    def update(self):
+        #print("Update")
+        self._draw_image()
+        self._draw_rect()
+        self._draw_hitbox()
 
 
 		
