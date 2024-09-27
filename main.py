7db39cb1041baa55e98a70536542d33cf4be82a0
Aze-M <archmageazekil@gmail.com>
2024-09-27 13:38:22 +0200

    moving away from canvas based ui due to pygame lmitations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 #from src.objs import item                       # This imports the player and item modules from the src.objs package.
 #from src.utils import damage_calc               # This imports the damage_calc module from the src.utils package
 from src.utils.player_controller import Player, Controller         # This imports the player_controller module from src.utils package 
+from src.utils.ui import WindowBase
 
 pygame.init()
 
@@ -13,12 +14,16 @@ height = 720
 display = pygame.display.set_mode((width, height))
 pygame.display.flip()
 
+#test ui window
+window = WindowBase(1, display, 100, 10, int(width/2), int(height*0.95), True)
+window._color = "black"
+
 # fps and clock generator
 clock = pygame.time.Clock()
 FPS = 60
 
 # color
-WHITE =(255, 255, 255)
+WHITE = (255, 255, 255)
 
 # create player and controller
 player = Player(400, 300, 5) # player object with start position and speed
@@ -43,6 +48,9 @@ while is_running:
     # fill the screen and move player
     display.fill(WHITE)
     player.draw(display)
+
+    #render ui after player logic always
+    window.update()
 
     # refresh screen
     pygame.display.flip()
--- src/utils/ui.py
+++ src/utils/ui.py
@@ -11,18 +11,17 @@ class WindowBase: #used for any window that can have subwindows and segments
     _color = pygame.Color(255,255,255,100)
     _unblitted = True
 
-    #takes ID, parent relative percentage witch and height, parent relative position, parent relative positon y, and either display or another rect as parent.
-    def __init__(self, _new_id: int, _parent_window: pygame.Surface, _perc_width: int= 100, _perc_height: int = 100, _pos_x: int = 0, _pos_y: int = 0, _centered = False):
+    #takes ID, parent, relative percentage witch and height, relative position x and positon y. should draw and keep track of rects created this way
+    def __init__(self, _new_id: int, _main_surface: pygame.Surface, _perc_width: int= 100, _perc_height: int = 100, _pos_x: int = 0, _pos_y: int = 0, _centered = False):
 
-        if not _parent_window:
+        if not _main_surface:
             raise RuntimeError("No parent given, can not use Canvas - style UI")
         
-        if type(_parent_window) is WindowBase:
-            self._parent = _parent_window._surface
-            _parent_window = _parent_window._surface
+        if type(_main_surface) is not pygame.Surface:
+            raise RuntimeError("Needs Surface to Draw UI!")
 
-        self._parent = _parent_window
-        self._parent_dimensions = [_parent_window.get_width(),_parent_window.get_height()]
+        self._parent = _main_surface
+        self._parent_dimensions = [_main_surface.get_width(),_main_surface.get_height()]
 
         self._id = _new_id
         self._width = int(self._parent_dimensions[0]*(_perc_width/100))
@@ -34,11 +33,10 @@ class WindowBase: #used for any window that can have subwindows and segments
 
         #preconstrcuct rectangle
         self._rect = pygame.Rect(_pos_x, _pos_y, self._width, self._height)
-        #use rectangle to make surface
-        self._surface = pygame.Surface((self._rect.width, self._rect.height))
-        self._surface.fill((0,0,0,0))
+
     
     def update(self):
-        if self._unblitted:
-            self._parent.blit(self._surface, (0,0))
         pygame.draw.rect(self._parent, self._color, self._rect)
+
+    def close(self): #destroys the window
+        del self
