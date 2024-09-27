08c09faa89fb862c7bbe031573359f6883b4a3b5
DoctorphatX <spatz.moritz@outllook.de>
2024-09-27 13:13:06 +0200

    player_controller erstellt und in main integriert

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
-from objs import creature                     # This imports the player and item modules from the src.objs package.
-from src.objs import item                       # This imports the player and item modules from the src.objs package.
-from src.utils import damage_calc               # This imports the damage_calc module from the src.utils package
+#from objs import creature                     # This imports the player and item modules from the src.objs package.
+#from src.objs import item                       # This imports the player and item modules from the src.objs package.
+#from src.utils import damage_calc               # This imports the damage_calc module from the src.utils package
+from src.utils.player_controller import Player, Controller         # This imports the player_controller module from src.utils package 
+
+pygame.init()
 
 
 width = 1280            #Game window height and width
@@ -9,6 +12,17 @@ height = 720
 
 display = pygame.display.set_mode((width, height))
 pygame.display.flip()
+
+# fps and clock generator
+clock = pygame.time.Clock()
+FPS = 60
+
+# color
+WHITE =(255, 255, 255)
+
+# create player and controller
+player = Player(400, 300, 5) # player object with start position and speed
+controller = Controller() #controller for input
 
 is_running = True       #This keeps the game window open
 while is_running:
@@ -19,7 +33,24 @@ while is_running:
             if event.key == pygame.K_ESCAPE:
                 is_running = False
 
+    
+    # get input
+    controller.handle_input()
 
+    # move the player
+    player.move(controller.keys)
+ 
+    # fill the screen and move player
+    display.fill(WHITE)
+    player.draw(display)
+
+    # refresh screen
+    pygame.display.flip()
+
+    # controll framerate
+    clock.tick(FPS)
+    
+pygame.quit()
 
 if __name__ == "__main__":                      # This line ensures that the code block is only executed if the script is run directly, not if it is imported as a module in another script.
     basE_item_test = item.Weapon(1,1,"Test",[0])  # This creates an instance of the ItemBase class from the item module with the parameters 1, 1, "Test".
