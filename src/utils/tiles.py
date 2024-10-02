13bc9e66163fb55a69555f22e56f8d3cd9d4c906
joergdeymann <joergdeymann@web.de>
2024-10-02 15:18:25 +0200

    Update tiles.py
    make it easy to use
    map can move

--- src/utils/tiles.py
+++ src/utils/tiles.py
@@ -15,32 +15,47 @@
 #    tiles.tile_item(pygame,2,"Road",'./assets/svg/road.svg'),
 #    tiles.tile_item(pygame,3,"Hill",'./assets/svg/hill.svg')
 # ]
-#
+# 
 # 2. Step Build up your Map
-#   At the momenent there is no Class for the List, I wil create one
-#   u=tiles.screen_tile(pygame,screen,5,tree,unique_entities[0])
-#   u.setPosition(300,200)
-#   u.setSize(200,200)
-#   u.setHitbox(20,34,160,156)
-#   main_entity.append(u)
-#
-# Late i will create a Json Array for entieies and you can get it by Name
-# unique_entities=[
-#    "tree":tiles.tile_item(pygame,1,"Tree",'./assets/svg/tree.svg'),
-#    "road":tiles.tile_item(pygame,2,"Tree",'./assets/svg/road.svg'),
+# level_tiles = [
+#   ["Tree"  ,100,100],
+#   ["Grass" ,100,200,64,64],
+#   ["Grass" ,100,300,128,128]
 # ]
-# and then have the Option
-# level1=tile_list(pygame,screen)
-# level1.add("tree",x,y,width,height,hit_x,hit_y,_hit_width,hit_height)
-#   hitbox entries are not required
-# or with 2 Parameters
-# level1.add("tree",dictionary_item)
-# or with
-# level1.add("tree",full_dictionary)
 #
-# so it will be easy to create a map
-
-# import tiles
+# EXAMPLE
+#
+# Parameters of the array:
+#   name    = not uniqe the name of the Item
+#   filename= of the Image we use, if we use 
+#   x       = x - position of the item
+#   y       = y - position of the item
+#   width   = x - optional new With   instead of the original 
+#   height  = x - optional new height instead of the orioginal
+#
+# unique-tiles=[
+#   ["Tree",'./assets/svg/tree.svg'  ,64,64, 5,12,52,52],
+#   ["Road",'./assets/svg/road.svg'  ,64,64,17, 0,32,64],
+#   ["Grass",'./assets/svg/grass.svg',64,64, 5,10,55,54]
+# ]
+#
+# Parameters of the array:
+#   name    = not uniqe the name of the Item
+#   x       = x - position of the item
+#   y       = y - position of the item
+#   width   = x - optional new With   instead of the original 
+#   height  = x - optional new height instead of the orioginal
+#
+# level_tiles = [
+#   ["Tree"  ,100,100],
+#   ["Grass" ,100,200,64,64],
+#   ["Grass" ,100,300,128,128]
+# ]
+# pygame.init()
+# pygamme_screen = pygame.display.set_mode((1280, 720))
+# myscreen = screen(pygame,pygame_screen)
+# myscreen.setTiles(unique_tiles)
+# myscreen.addItems(level_tiles)
 
 class tile_list:
     _pygame=None #Pygame 
@@ -60,7 +75,7 @@ class tile_list:
 
     def addItems(self,items):
         for item in items:
-            #        name    src     x       y       Hit_x   Hit_y   Hit_w   Hit_h
+            #        name    src     width   height      Hit_x   Hit_y   Hit_w   Hit_h
             self.add(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7])
     
     def getItem(self,name):
@@ -91,8 +106,6 @@ class tile_item:
         self._src   = src
         self._image = self.__load(pygame,src)
 
-    
-
     def __load(self,pygame,src):
         #pygame.image.load(os.path.join('data', src))
         return pygame.image.load(src)
@@ -119,16 +132,9 @@ class tile_item:
         else:
             self._width=w
         if (height !=None):
-            self._height:height
+            self._height=height
         else:
             self._height=h
-
-    
-
-
-    
-
-
 
 class screen:
     _pygame=None    #Empty Python container
@@ -137,44 +143,135 @@ class screen:
     _clock=None     #Timer
     _item_list={}   #All unique Items in Original size 
     _id:int = 0     #unique ID
+    _offset_x=50    #Posiiton of map_area
+    _offset_y=100   #Position of Map area
+    xd=0            #moving Direction x 
+    yd=0            #movinf Direction y
 
     def __init__(self,pygame,screen):
         self._pygame  = pygame
         self._screen  = screen
     
+
+    #Global Unique Entities/Tiles
     def setTiles(self,tiles):
-        self._tile_list=tiles
-        #print (tiles.getItem("Tree")._width)
-        #print (self._tile_list.getItem("Tree")._width)
+        self._tile_list=tile_list(self._pygame)
+        self._tile_list.addItems(tiles)    
     
 
-    def add(self,name,x,y,width=None,height=None,hit_x=None,hit_y=None,hit_width=None,hit_height=None,):
+    #
+    # PUBLIC
+    # 
+    # appends a new Item to the screen
+    #
+    # SYNTAX
+    #   myscreen.add("Tree",100,150,64,64)
+    #   myscreen.add("Grass",300,250)
+    #
+    # Parameters:
+    #   name    = not uniqe the name of the Item
+    #   x       = x - position of the item
+    #   y       = y - position of the item
+    #   width   = x - optional new With   instead of the original 
+    #   height  = x - optional new height instead of the orioginal
+    #
+    def add(self,name,x,y,width=None,height=None,rotation=None):
         self._id+=1
+        tile=self._tile_list.getItem(name)
 
-        item=screen_tile(self._pygame, self._screen, self._id, self._tile_list.getItem(name))
+        item=screen_tile(self,tile)
+        #item=screen_tile(self._pygame, self._screen, self, self._id, tile)
         item.setPosition(x,y)
+
         if (width and height):
             item.setSize(width,height)
-        
-        if (hit_x and hit_y and hit_width and hit_height):
-            item.setHitbox(width,height)
+
+        if(rotation):
+            item.setRotation(rotation)
 
         self._item_list[self._id]=item    
-        print (item._width)
-        print (self._item_list[self._id]._width)
-
         return item
+    
+    #
+    # PUBLIC
+    # 
+    # SYNTAX
+    #   myscreen.addItems(dictionary)
+    #
+    # Parameters of the array:
+    #   name    = not uniqe the name of the Item
+    #   x       = x - position of the item
+    #   y       = y - position of the item
+    #   width   = x - optional new With   instead of the original 
+    #   height  = x - optional new height instead of the orioginal
+    #   infollowing format:
+    #   dictionary = [
+    #       ["Tree" ,100,100],
+    #       ["Road" ,100,200,64,64],
+    #       ["Grass",100,300,128,128]
+    #   ]
+    #
+    def addItems(self,items):
+        for item in items:
+            self.addItem(item)
 
+    #
+    # PUBLIC
+    # 
+    # SYNTAX
+    #   myscreen.addItem(array)
+    #
+    # Parameters of the array:
+    #   name    = not uniqe the name of the Item
+    #   x       = x - position of the item
+    #   y       = y - position of the item
+    #   width   = x - optional new With   instead of the original 
+    #   height  = x - optional new height instead of the orioginal
+    #   rotation= Start Rotation
+    #
+    #   infollowing format:
+    #   array = ["Tree" ,100,100]
+    #   array = ["Tree" ,100,100,32,64]
+    #
+    def addItem(self,item):
+        item.append(5-len(item))
+        
+        #for i in range (2,5):
+        #    item[i]=0
+        self.add(item[0],item[1],item[2],item[3],item[4],item[5])
+
+    #
+    # PUBLIC
+    # 
+    # SYNTAX
+    #   item=myscreen.getItem(index)
+    #
+    # PARAMETERS:
+    #   index: the unique Number of the object
+    # 
+    # Returns
+    #   item    = the Item-object of the list
+    #
     def getItem(self,index):
         return self._item_list[index]
         
+
+    # 
+    # PUBLIC
+    #
+    # Updates the Graphic
+    #
+    # SYNTAX
+    #   myscreen.update()
+    #  
     def update(self):
-        print ("FOR")
-        print(self._item_list[1]._speed)
-        print(vars(self._item_list[1].title_item))
-        for item in self._item_list:
-            print(item._width)
-            #item.update()  
+        for item in self._item_list.values():
+            self.move()
+            item.update()  
+
+    def move(self):
+        self._offset_x+=self.xd
+        self._offset_y+=self.yd
 
 
 # Tile = Item auf dem Bildschirm , Entity = Interagierendes Item
@@ -190,39 +287,56 @@ class screen_tile:
     _name:str = ""            #ZB "Baum"
     _x:int = 0                #Position X
     _y:int = 0                #Position Y
-    _width:int = 0            #Width 
-    _height:int = 0           #Height
+    _width:int = None         #Width 
+    _height:int = None        #Height
     _scale:float = 1          #Scale Factor
+    _startrotation:int = 0    #Rotation          
     _tile=None                #has the src of the Image
 
-	
-	
     _dx:int =0				  #movement per 1/60s  -1 left +1 right 0 no move
     _dy:int =0                #movement per 1/60s  -1 up   +1 down  0 no move
     _speed:float =1           #Speed for the Movement (Steps to move or times)
+
     _hitbox_x:int = None                #Collision Position X
     _hitbox_y:int = None                #Collision Position Y
     _hitbox_width:int = None            #Collision Width 
     _hitbox_height:int = None           #Collision Height
-    __TRANSPARENT = (0,0,0,0)        #Transparenter Hintergrund 0-255
-    __BLUE        = (255,255,0)    #Transparenter Hintergrund 0-255
-    __RED         = (255,0,0,128)    #Transparenter Hintergrund 0-255
-	
+    _original_width = None              #from tile or other
+    _original_height = None             #from tile or other
+    _original_hitbox_x:int = None       #Collision Position X
+    _original_hitbox_y:int = None       #Collision Position Y
+    _original_hitbox_width:int = None   #Collision Width 
+    _original_hitbox_height:int = None  #Collision Height
+    
+    __TRANSPARENT = (0,0,0,0)           #Transparenter Hintergrund 0-255
+    __BLUE        = (255,255,0)         #Transparenter Hintergrund 0-255
+    __RED         = (255,0,0,128)       #Transparenter Hintergrund 0-255
+
+
+
+
 	# Erstelle eine Surface mit Transparenz
 	#transparent_surface = pygame.Surface((rect_width, rect_height), pygame.SRCALPHA)
 	#transparent_surface.fill(BLUE)  # Fülle die Surface mit transparenter Farbe
 	# Zeichne die transparente Surface
     #screen.blit(transparent_surface, (rect_x, rect_y))
     
+    #item=screen_tile(self._pygame, self._screen, self, self._id, tile)
 
-    def __init__(self,pygame,screen,id,tile=None):
-        self._id=id
+    def __init__(self,parent,tile):
+        #self._id=id
+        #self._tile=tile
+        #self._pygame=pygame
+        #self._screen=screen
+
+        self._parent=parent
+        self._id=parent._id
         self._tile=tile
-        self._pygame=pygame
-        self._screen=screen
+        self._pygame=parent._pygame
+        self._screen=parent._screen
+
         if (tile):
             self.setSize(tile._width, tile._height)
-            
 
     def setId(self,id):
         self._id=id
@@ -238,43 +352,68 @@ class screen_tile:
         if (self._hitbox_y == None):
             self._hitbox_y=0
 
-    def setSize(self,width,height):
-        w=self._width
-        h=self._height
+    def _storeOriginalSize(self):
+        if self._tile:
+            if self._original_width == None:
+                self._original_width=self._tile._width
+            if self._original_height == None:
+                self._original_height=self._tile._height
+        if self._original_width == None:
+            self._original_width=self._width
+        if self._original_height == None:
+            self._original_height=self._height
 
+    def _storeOriginalHitbox(self):
+        if self._tile:
+            if self._original_hitbox_width == None:
+                self._original_hitbox_width=self._tile._hitbox_width
+            if self._original_hitbox_height == None:
+                self._original_hitbox_height=self._tile._hitbox_height
+            if self._original_hitbox_x == None:
+                self._original_hitbox_x=self._tile._hitbox_x
+            if self._original_hitbox_y == None:
+                self._original_hitbox_y=self._tile._hitbox_y
+
+        if self._original_hitbox_width == None:
+            self._original_hitbox_width=self._hitbox_width
+        if self._original_hitbox_height == None:
+            self._original_hitbox_height=self._hitbox_height
+        if self._original_hitbox_x == None:
+            self._original_hitbox_x=self._tile._hitbox_x
+        if self._original_hitbox_y == None:
+            self._original_hitbox_y=self._tile._hitbox_y
+
+    def setSize(self,width,height):
+        #w=self._width
+        #h=self._height
+
+    
         self._width=width
         self._height=height
-        if (self._hitbox_width == None):
-            self._hitbox_width=width
-        if (self._hitbox_height == None):
-            self._hitbox_height=height
 
-        
-        self.resizeHitbox(w,h,width,height)
+        self._storeOriginalSize()
+        self.resizeHitbox(width,height)
 
-        #resize Hitbox
-        #self._hitbox_width=self._hitbox_width*width/w
-        #self._hitbox_height=self._hitbox_height*height/h
-        #self._hitbox_x=self._hitbox_x*width/h
-        #self._hitbox_y=self._hitbox_y*height/h
     
     #resize Hitbox from w,h to width height
-    def resizeHitbox(self,w,h,width,height):
-        if w == 0 or h == 0:
+    # 2 Angaben nur die Neue
+    def resizeHitbox(self,width,height):
+        if (self._hitbox_width == None):
+            self.setHitbox(self._x,self._y,self._width,self._height)
+        if self._width == 0 or self._height == 0:
             return
 
-        print (w)
-        print (width)
-        self._hitbox_width=self._hitbox_width*width/w
-        self._hitbox_height=self._hitbox_height*height/h
-        self._hitbox_x=self._hitbox_x*width/h
-        self._hitbox_y=self._hitbox_y*height/h
+        self._hitbox_width   =self._original_hitbox_width*width/self._width
+        self._hitbox_height  =self._original_hitbox_height*height/self._height
+        self._hitbox_x       =self._original_hitbox_x*width/self._width
+        self._hitbox_y       =self._original_hitbox_y*height/self._height
 
     def setHitbox(self,x,y,width,height):
         self._hitbox_x=x
         self._hitbox_y=y
         self._hitbox_width=width
         self._hitbox_height=height
+        self._storeOriginalHitbox()
 
     def setScale(self,scale):
         self._scale=scale
@@ -288,6 +427,9 @@ class screen_tile:
     
     def setTitle(self,tile):
         self._tile=tile
+
+    def setRotation(self,r):
+        self._startrotation=r
 
     def move(self,x,y):
 	    #remove old from screen or show animation
@@ -303,34 +445,53 @@ class screen_tile:
 
     def collides():
         pass
-    
+
+
+    def rotateRect(self,x,y,w,h,color,rot):
+        pygame=self._pygame
+        rect_surface = pygame.Surface((w, h), pygame.SRCALPHA)
+        pygame.draw.rect(rect_surface, color, (0, 0, w, h), 2)
+        rotated_rect = pygame.transform.rotate(rect_surface, rot)
+        rect = rotated_rect.get_rect(center=(x + w // 2, y + h // 2))
+        screen=self._screen
+        screen.blit(rotated_rect, rect.topleft)
+
     def _draw_rect(self):
-        x=self._x * self._scale
-        y=self._y * self._scale
+        offset_x=self._parent._offset_x
+        offset_y=self._parent._offset_y
+        x=(self._x - offset_x) * self._scale
+        y=(self._y - offset_y) * self._scale
         w=self._width * self._scale
         h=self._height * self._scale
-        screen=self._screen
-        # self._pygame.draw.rect(screen,self.__BLUE,(x,y,w,h),2)    
-        self._pygame.draw.rect(screen,self.__BLUE,(x,y,w,h),2)
+        self.rotateRect(x,y,w,h,self.__BLUE,self._startrotation)
+
 
     def _draw_hitbox(self):
-        x=(self._x+self._hitbox_x) * self._scale
-        y=(self._y+self._hitbox_y) * self._scale
+        offset_x=self._parent._offset_x
+        offset_y=self._parent._offset_y
+        x=(self._x+self._hitbox_x - offset_x) * self._scale
+        y=(self._y+self._hitbox_y - offset_y) * self._scale
         w=self._hitbox_width * self._scale
         h=self._hitbox_height * self._scale
-        screen=self._screen
-        self._pygame.draw.rect(screen,self.__RED,(x,y,w,h),2)
+        self.rotateRect(x,y,w,h,self.__RED,self._startrotation)
 
     def _draw_image(self):
-        x=self._x * self._scale
-        y=self._y * self._scale
-        w=self._width
-        h=self._height
         if (self._tile._image == None):
             return
+        offset_x=self._parent._offset_x
+        offset_y=self._parent._offset_y
+        x=(self._x - offset_x) * self._scale
+        y=(self._y - offset_y) * self._scale
+        w=self._width
+        h=self._height
         
         image=self._tile._image
         image=self._pygame.transform.scale(image, (w, h))
+        
+        rot=self._startrotation #in grad
+        if rot:
+            image=self._pygame.transform.rotate(image, rot)
+        
         #rotated_image = pygame.transform.rotate(image, 45)
         screen=self._screen
         screen.blit(image, (x, y)) 
@@ -342,9 +503,11 @@ class screen_tile:
         self._draw_image()
         self._draw_rect()
         self._draw_hitbox()
+        #print (screen._offset_x)
+        #print (self._parent._offset_x)
 
     def getX(self):
-        return self._x
+        return 
 
 		
 
